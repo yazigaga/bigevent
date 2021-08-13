889223fa04a89f555c736b503bb0b6833d9e29c9
xy <31774572@qq.com>
2021-08-13 19:26:23 +0800

    完成登录和注册功能

--- code/assets/js/baseAPI.js
+++ code/assets/js/baseAPI.js
@@ -1,3 +1,4 @@
-$.ajaxprefilter(function(options) {
-    options.url = '"http://www.liulongbin.top:3008/' + options.url;
+$.ajaxPrefilter(function(options) {
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    options.url = "http://www.liulongbin.top:3008/" + options.url;
 });
